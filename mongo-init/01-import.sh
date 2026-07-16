9f82b0a09534282ba039b453a743d6257cb89f2c
Simran-arch-alt <simrankarmacharya79@gmail.com>
2026-07-16 11:41:51 +0545

    Keep only python-engine for data analytics

--- mongo-init/01-import.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-echo "Importing jobs dataset into capstone_db.jobs..."
-mongoimport --db capstone_db --collection jobs --type csv --headerline --drop --file /seed/jobs_dataset_skills_final.csv
-echo "Import complete."
